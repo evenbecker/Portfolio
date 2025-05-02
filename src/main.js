0f64d13b8e8053bbc09c81ac6e9bbea8117cc96b
Even Becker <torettocustom@gmail.com>
2025-05-02 18:59:23 +0200

    bootstrap libs

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-import "/home/even/.global-node-modules/node_modules/bootstrap/dist/css/bootstrap.min.css";
+//import "/home/even/.global-node-modules/node_modules/bootstrap/dist/css/bootstrap.min.css";
 //const bootstrap = require('bootstrap');
 
 createApp(App).mount('#app')
