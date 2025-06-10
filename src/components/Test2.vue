ce1f18bd304fb9bea20af01fceacea63679a87f8
Even Becker <torettocustom@gmail.com>
2025-06-10 19:13:33 +0200

    flex fixed

--- src/components/Test2.vue
+++ src/components/Test2.vue
@@ -17,11 +17,21 @@
       <div class = "absol">
         <p class="verdanaBold">Some alt rock bands</p>
         <p>All right reserved to the photographs/labels.</p>   
-        <div class="fun-font">
-          <p>Jumpdaf*ckup!
-            <img style="display: inline; margin: 45px;" alt="guitar" width="200px" height="auto" src="../assets/pictures/guitar2.png">
-          </p>
-        </div>
+  
+    <div class="section1">           
+      <div class="animated_title">
+        <div class="text_top">
+          <div>
+            <span class="text-bounce animate">Jumpdafuckup!</span>
+            <!--<span class="text-bounce">guitar</span>-->
+          </div>
+        </div>      
+      </div>
+         
+        <img class="guitar" alt="guitar" src="../assets/pictures/guitar2.png">
+                         
+    </div>                    
+  
         <h2>Human Waste Project</h2>
         <img  alt="Human Waste Project" width="65%" height="auto" src="../assets/pictures/HumanWP.webp">
         <div class="spacer"></div>  
@@ -98,6 +108,15 @@
     </script>
     <!-- Add "scoped" attribute to limit CSS to this component only -->
     <style scoped>
+    .guitar{
+      /*display: inline;*/
+      position: absolute;
+      z-index: 1;
+      margin-left: 150px;
+      /*width: 55px;*/
+      height: 70px;
+
+}
 #myProgress {
   margin: auto;
   width: 40%;
@@ -151,4 +170,327 @@
       margin-left: auto;
       margin-right: auto;
     }
+
+
+
+*::-webkit-scrollbar {
+      width: 10px;
+    }
+
+    *::-webkit-scrollbar-track {
+      background: whitesmoke;
+    }
+
+    *::-webkit-scrollbar-thumb {
+      background: linear-gradient(90deg, rgba(120,120,121,1) 0%, rgba(136,136,136,1) 100%);
+      border-radius: 10px;
+    } 
+
+
+    .section1 {
+      font-family:'Poppins', sans-serif;
+      display: flex;            
+      position: relative;
+      /*justify-content: center;
+      align-items: center;*/
+    }
+
+    
+    .animated_title {
+      color: #222;
+      font-family: 'Poppins', sans-serif;
+      letter-spacing: 1.2px;
+      height: 15vmax;
+      position: relative;
+      width: 33vmax;
+      /*margin: 0 auto;*/
+      display: flex;
+      left: 40%;
+      /*right: 20%;*/
+      z-index: 2;
+    }
+
+    .animated_title > div {
+      height: 85%;/*70% */
+      overflow: hidden;
+      position: absolute;
+      width: 100%;
+    }
+
+    .animated_title > div div {
+      font-size: 4vmax;
+      font-weight: 580;
+      padding: 2vmin 0;
+      position: absolute;
+      
+    }
+
+    .animated_title > div div span {
+      display: block;
+      
+    }
+
+    .animated_title > div.text_top {
+      border-bottom: .8vmin solid #747474;
+      width: 92%;
+      animation: showBottomText 0.4s;
+      animation-delay: 0.1s;
+      top: 0;
+    }
+
+    .animated_title > div.text_top div {
+      animation: showTopText 0.7s;
+      animation-delay: 0.5s;
+      animation-fill-mode: forwards;
+      bottom: 0;
+      transform: translate(0, 100%);
+    }
+
+    .animated_title > div.text_top div span:first-child {
+      color: #747474;
+    }
+
+    .animated_title > div.text_top div span:first-child:hover{
+      color: #F34F1C;
+    }
+    .animated_title > div.text_top div span:nth-child(2):hover{
+      color: #FFBA01;
+    }
+
+   
+
+    .bounce-in-right {
+      animation: bounce-in-right 1.2s cubic-bezier(0.455, 0.030, 0.515, 0.955) both;
+      animation-iteration-count: infinite;
+    }
+
+    .bounce-in-top {
+      animation: bounce-in-top 1.3s cubic-bezier(0.455, 0.030, 0.515, 0.955) both;
+      animation-iteration-count: infinite;
+    }
+
+    .bounce-in-left {
+      animation: bounce-in-left 1.1s cubic-bezier(0.455, 0.030, 0.515, 0.955) both;
+      animation-iteration-count: infinite;
+    }
+
+    .bounce-in-bottom {
+      animation: bounce-in-bottom 1.2s cubic-bezier(0.455, 0.030, 0.515, 0.955) both;
+      animation-iteration-count: infinite;
+    }
+
+    
+    .text-bounce {
+      transform: scaleY(1) translateY(0px);
+      transform-origin: 50% 100%;
+      transition: .3s ease-in-out;
+    }
+    .animate {
+      animation: bouncy 1s infinite;/*forwards;*/
+    }
+    :hover {
+      cursor: default;
+    }
+
+    @keyframes showTopText {
+      0% {
+        transform: translate3d(0, 100%, 0);
+      }
+      40%, 60% {
+        transform: translate3d(0, 50%, 0);
+      }
+      100% {
+        transform: translate3d(0, 0, 0);
+      }
+    }
+    @keyframes showBottomText {
+      0% {
+        transform: translate3d(0, -100%, 0);
+      }
+      100% {
+        transform: translate3d(0, 0, 0);
+      }
+    }
+
+    @keyframes bouncy{
+      0%{transform: scaleY(1) translateY(0px);}
+      25%{transform: scaleY(.5) translateY(20px);}
+      50%{transform: scaleY(1.1) translateY(-20px);}
+      75%{transform: scaleY(.8) translateY(10px);}
+      100%{transform: scaleY(1) translateY(0px);}
+    }
+
+    @keyframes bounce-in-right {
+      0% {
+        transform: translateX(120px);
+        animation-timing-function: ease-in;
+        opacity: 0;
+      }
+      38% {
+        transform: translateX(0);
+        animation-timing-function: ease-out;
+        opacity: 1;
+      }
+      55% {
+        transform: translateX(68px);
+        animation-timing-function: ease-in;
+      }
+      72% {
+        transform: translateX(0);
+        animation-timing-function: ease-out;
+      }
+      81% {
+        transform: translateX(32px);
+        animation-timing-function: ease-in;
+      }
+      90% {
+        transform: translateX(0);
+        animation-timing-function: ease-out;
+      }
+      95% {
+        transform: translateX(8px);
+        animation-timing-function: ease-in;
+      }
+      100% {
+        transform: translateX(0);
+        animation-timing-function: ease-out;
+      }
+    }
+    @keyframes bounce-in-top {
+      0% {
+        transform: translateY(-120px);
+        animation-timing-function: ease-in;
+        opacity: 0;
+      }
+      38% {
+        transform: translateY(0);
+        animation-timing-function: ease-out;
+        opacity: 1;
+      }
+      55% {
+        transform: translateY(-65px);
+        animation-timing-function: ease-in;
+      }
+      72% {
+        transform: translateY(0);
+        animation-timing-function: ease-out;
+      }
+      81% {
+        transform: translateY(-28px);
+        animation-timing-function: ease-in;
+      }
+      90% {
+        transform: translateY(0);
+        animation-timing-function: ease-out;
+      }
+      95% {
+        transform: translateY(-8px);
+        animation-timing-function: ease-in;
+      }
+      100% {
+        transform: translateY(0);
+        animation-timing-function: ease-out;
+      }
+    }
+    @keyframes bounce-in-left {
+      0% {
+        transform: translateX(-120px);
+        animation-timing-function: ease-in;
+        opacity: 0;
+      }
+      38% {
+        transform: translateX(0);
+        animation-timing-function: ease-out;
+        opacity: 1;
+      }
+      55% {
+        transform: translateX(-68px);
+        animation-timing-function: ease-in;
+      }
+      72% {
+        transform: translateX(0);
+        animation-timing-function: ease-out;
+      }
+      81% {
+        transform: translateX(-28px);
+        animation-timing-function: ease-in;
+      }
+      90% {
+        transform: translateX(0);
+        animation-timing-function: ease-out;
+      }
+      95% {
+        transform: translateX(-8px);
+        animation-timing-function: ease-in;
+      }
+      100% {
+        transform: translateX(0);
+        animation-timing-function: ease-out;
+      }
+    }
+    @keyframes bounce-in-bottom {
+      0% {
+        transform: translateY(120px);
+        animation-timing-function: ease-in;
+        opacity: 0;
+      }
+      38% {
+        transform: translateY(0);
+        animation-timing-function: ease-out;
+        opacity: 1;
+      }
+      55% {
+        transform: translateY(65px);
+        animation-timing-function: ease-in;
+      }
+      72% {
+        transform: translateY(0);
+        animation-timing-function: ease-out;
+      }
+      81% {
+        transform: translateY(28px);
+        animation-timing-function: ease-in;
+      }
+      90% {
+        transform: translateY(0);
+        animation-timing-function: ease-out;
+      }
+      95% {
+        transform: translateY(8px);
+        animation-timing-function: ease-in;
+      }
+      100% {
+        transform: translateY(0);
+        animation-timing-function: ease-out;
+      }
+    }
+
+    @keyframes image-bounce-in-right {
+      0% {
+        transform: translateX(0);
+        opacity: 0;
+      }
+      38% {
+        transform: translateX(10px);
+        opacity: 1;
+      }
+      55% {
+        transform: translateX(5px);
+      }
+      72% {
+        transform: translateX(15px);
+      }
+      81% {
+        transform: translateX(10px);
+      }
+      90% {
+        transform: translateX(20px);
+      }
+      100% {
+        transform: translateX(10px);
+      }
+    }
+
+  
+
     </style>
